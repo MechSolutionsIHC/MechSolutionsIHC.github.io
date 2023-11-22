8a9ed9e38b5b754736e9a6368a245202dca40828
Rodrigo <129321935+RodrLH@users.noreply.github.com>
2023-11-22 00:36:28 -0500

    Update Feedback.js

--- assets/scripts/Feedback.js
+++ assets/scripts/Feedback.js
@@ -1,50 +1,7 @@
-
-var bugImg = document.getElementById("bugImg");
-var feedbackImg = document.getElementById("feedbackImg");
-var supportImg = document.getElementById("supportImg");
-var cuadroTexto = document.getElementById("cuadroTexto");
-var textoArea = document.getElementById("texto");
-var cancelarBtn = document.getElementById("cancelarBtn");
-var enviarBtn = document.getElementById("enviarBtn");
- 
-bugImg.addEventListener("click", function() {
-    mostrarCuadroTexto("Reportar Error");
-});
-
-feedbackImg.addEventListener("click", function() {
-    mostrarCuadroTexto("Sugerencias");
-});
-
-supportImg.addEventListener("click", function() {
-    mostrarCuadroTexto("Soporte");
-});
-
-function mostrarCuadroTexto(titulo) {
-    
-    cuadroTexto.querySelector("h2").textContent = titulo;
-    
-   
-    cuadroTexto.style.display = "block";
- 
-    textoArea.disabled = true;
- 
-    cancelarBtn.disabled = false;
-    enviarBtn.disabled = false;
-}
-
- 
-cancelarBtn.addEventListener("click", function() {
- 
-    cuadroTexto.style.display = "none";
-
-   
-    textoArea.disabled = false;
-});
- 
-enviarBtn.addEventListener("click", function() {
-     
-    cuadroTexto.style.display = "none";
-
- 
-    textoArea.disabled = false;
-});
+window.addEventListener("load",function (){
+    document.querySelector(".form").addEventListener("submit",function (event){
+        event.preventDefault();
+        alert("¡Gracias por escribirnos!");
+        this.reset();
+    })
+})
